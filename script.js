69a67a8d68057a90f7f18d8773c64fddc55ae170
Esther Bastos <100881440+estherfernandesb@users.noreply.github.com>
2024-04-29 22:07:24 -0300

    Update script.js
    Now you can use your tablet to draw, because I added the touch option.

--- script.js
+++ script.js
@@ -19,17 +19,30 @@ document.addEventListener('DOMContentLoaded', function() {
   function draw(e) {
     if (!isDrawing) return;
 
+    let x, y;
+    if (e.touches && e.touches.length > 0) {
+      x = e.touches[0].clientX;
+      y = e.touches[0].clientY;
+    } else {
+      x = e.clientX;
+      y = e.clientY;
+    }
+
     ctx.lineWidth = 2;
     ctx.lineCap = 'round';
     ctx.strokeStyle = '#000';
 
-    ctx.lineTo(e.clientX, e.clientY);
+    ctx.lineTo(x, y);
     ctx.stroke();
     ctx.beginPath();
-    ctx.moveTo(e.clientX, e.clientY);
+    ctx.moveTo(x, y);
   }
 
   canvas.addEventListener('mousedown', startDrawing);
   canvas.addEventListener('mouseup', stopDrawing);
   canvas.addEventListener('mousemove', draw);
+
+  canvas.addEventListener('touchstart', startDrawing);
+  canvas.addEventListener('touchend', stopDrawing);
+  canvas.addEventListener('touchmove', draw);
 });
